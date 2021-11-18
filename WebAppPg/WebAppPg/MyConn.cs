eb68ee3c8d900e9051758df6d148648df56d107e
AnnaZiankevich <94486116+AnnaZiankevich@users.noreply.github.com>
2021-11-18 17:03:04 +0300

    New AppSettings, GetConnection, OpenConnection, FreeConnection

--- WebAppPg/WebAppPg/MyConn.cs
+++ WebAppPg/WebAppPg/MyConn.cs
@@ -1,15 +1,41 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Npgsql;
 
 namespace WebAppPg
 {
-    public class MyConn
+    public static class MyConn
     {
+        private static readonly IConfiguration config;
+        static NpgsqlConnection connection;
+        static Boolean isInitialized = false;
+        //IDecorator
+        //Decorator
 
+        static public NpgsqlConnection GetConnection() 
+        {
+            if (!isInitialized) OpenConnection();
+            return connection;
+            //return new NpgsqlConnection(dbConn);
+        }
+
+        static public void OpenConnection()
+        {
+            string dbConn = config.GetValue<string>("ConnectionStrings:PGDB");
+            connection = new NpgsqlConnection(dbConn);
+            connection.Open();
+            isInitialized = true;
+        }
+        
+        public static void FreeConnection()
+        {
+            if (isInitialized) connection.Close();
+        }
     }
 }
