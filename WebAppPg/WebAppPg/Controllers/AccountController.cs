49ba448077897fc13743a956dcd05819a2d72a9b
AnnaZiankevich <94486116+AnnaZiankevich@users.noreply.github.com>
2021-12-09 21:53:58 +0300

    New AccountModel, AccountController, List

---
+++ WebAppPg/WebAppPg/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using WebAppPg.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Npgsql;
+using NpgsqlTypes;
+using System.Data;
+using System.Security.Claims;
+
+namespace WebAppPg.Controllers
+{
+    public class AccountController : Controller
+    {
+        static List<Account> accountsList = new List<Account>();
+
+        [HttpGet]
+        public IActionResult List(int id)
+        {
+            string userId = HttpContext.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            NpgsqlConnection conn = DbConn.Instance.GetMainConnection(int.Parse(userId));
+            using (conn)
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand("select id, name, account_type_code, is_active, currency_code" +
+                                                        " from sb.accounts where account_owner_id = @accOwnerId order by name", conn);
+                cmd.Parameters.AddWithValue("accOwnerId", NpgsqlDbType.Integer, id);
+                accountsList.Clear();
+                NpgsqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var account = new Account 
+                    { 
+                        id = rdr.GetInt32("id"),
+                        name = rdr.GetString("name"),
+                        account_type_code = rdr.GetString("account_type_code"),
+                        account_owner_id = id,
+                        is_active = rdr.GetBoolean("is_active"),
+                        currency_code = rdr.GetString("currency_code")
+                    };
+
+                    accountsList.Add(account);
+                    //Read(account, rdr);
+                }
+                DbConn.Instance.FreeConnection(conn);
+            }
+            return View(accountsList);
+        }
+        [HttpGet]
+        public IActionResult ListExt([Bind("accOwnerId")] int accOwnerid)
+        {
+            string userId = HttpContext.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            NpgsqlConnection conn = DbConn.Instance.GetMainConnection(int.Parse(userId));
+            using (conn)
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand("select id, name, account_type_code, is_active, currency_code" +
+                                                        " from sb.accounts where account_owner_id = @accOwnerId order by name", conn);
+                cmd.Parameters.AddWithValue("accOwnerId", NpgsqlDbType.Integer, accOwnerid);
+                accountsList.Clear();
+                NpgsqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var account = new Account
+                    {
+                        id = rdr.GetInt32("id"),
+                        name = rdr.GetString("name"),
+                        account_type_code = rdr.GetString("account_type_code"),
+                        account_owner_id = accOwnerid,
+                        is_active = rdr.GetBoolean("is_active"),
+                        currency_code = rdr.GetString("currency_code")
+                    };
+
+                    accountsList.Add(account);
+                    //Read(account, rdr);
+                }
+                DbConn.Instance.FreeConnection(conn);
+            }
+            return View(accountsList);
+        }
+     }
+}
---
+++ WebAppPg/WebAppPg/Models/AccountModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppPg.Models
+{
+    public class Account
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string account_type_code { get; set; }
+        public List<AccountType> accountTypesList { get; set; }
+        public int account_owner_id { get; set; }
+        //public List<AccountOwner> accountOwnersList { get; set; }
+        public Boolean is_active { get; set; }
+        public string currency_code { get; set; }
+        public List<CurrencyCode> currencyCodesList { get; set; }
+        public int row_version { get; set; }
+    }
+
+    public class AccountType
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class CurrencyCode
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+}
